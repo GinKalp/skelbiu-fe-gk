f8570f51ee7384e343fd2091c08d7e39521f58bd
GinKalp <80583818+GinKalp@users.noreply.github.com>
2021-12-18 15:17:06 +0200

    did favorites sorting

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,13 @@ import css from "./HomePage.module.css";
 import ListingsList from "../components/Listings/ListingsList";
 import { getFetchAuth } from "../helpers/fetchHelper";
 import { useAuthCtx } from "../store/authContext";
+import Button from "../components/UI/Button/Button";
 
 const HomePage = () => {
   const [listings, setListings] = useState([]);
   const { authData, logout } = useAuthCtx();
+  const [isFilter, setIsFilter] = useState(false);
+  const [allListings, setAllListings] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -15,6 +18,7 @@ const HomePage = () => {
       if (gotData.error === "token expired/invalid") {
         logout();
       }
+      setAllListings(gotData?.data);
       setListings(gotData?.data);
       // console.log(authData.token);
     })();
@@ -23,9 +27,24 @@ const HomePage = () => {
       setListings([]);
     };
   }, [authData.token, logout]);
+
+  const onFilter = () => {
+    if (!isFilter) {
+      const filteredArr = listings.filter((item) => item.fav_user);
+      console.log(filteredArr);
+      setListings(filteredArr);
+      setIsFilter(!isFilter);
+    } else {
+      setListings(allListings);
+      setIsFilter(!isFilter);
+    }
+  };
   return (
     <div className={css.main}>
       <h2 className={`${css.title}`}>Welcome to our Page</h2>
+      <Button onClick={onFilter}>
+        {!isFilter ? "Show favorites" : "Show All"}
+      </Button>
       <ListingsList listings={listings} />
     </div>
   );
