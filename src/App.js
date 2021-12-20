b8e1fc8686cc98549cebaadaa22b7c47d3a98346
GinKalp <80583818+GinKalp@users.noreply.github.com>
2021-12-20 11:11:55 +0200

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import "./App.css";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import NavBar from "./components/NavBar/NavBar";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import MyAccountPage from "./pages/MyAccountPage";
-import Footer from "./components/Footer/Footer";
 import { useAuthCtx } from "./store/authContext";
+
+const NavBar = React.lazy(() => import("./components/NavBar/NavBar"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const RegisterPage = React.lazy(() => import("./pages/RegisterPage"));
+const MyAccountPage = React.lazy(() => import("./pages/MyAccountPage"));
+const Footer = React.lazy(() => import("./components/Footer/Footer"));
 
 function App() {
   const { isLoggedIn } = useAuthCtx();
   return (
     <div className="App">
       <Toaster />
-      <NavBar />
-      <Switch>
-        <Route path={"/login"}>
-          <LoginPage />
-        </Route>
-        <Route path={"/register"}>
-          <RegisterPage />
-        </Route>
-        {isLoggedIn && (
-          <Route path={"/my-account"}>
-            <MyAccountPage />
+      <React.Suspense fallback={<p>Loading...</p>}>
+        <NavBar />
+        <Switch>
+          <Route path={"/login"}>
+            <LoginPage />
           </Route>
-        )}
-        <Route exact path={"/"}>
-          <HomePage />
-        </Route>
-        <Route path={"*"}>
-          <h1 className={"container"}>Oops, page not found.</h1>
-        </Route>
-      </Switch>
-      <Footer />
+          <Route path={"/register"}>
+            <RegisterPage />
+          </Route>
+          {isLoggedIn && (
+            <Route path={"/my-account"}>
+              <MyAccountPage />
+            </Route>
+          )}
+          <Route exact path={"/"}>
+            <HomePage />
+          </Route>
+          <Route path={"*"}>
+            <h1 className={"container"}>Oops, page not found.</h1>
+          </Route>
+        </Switch>
+        <Footer />
+      </React.Suspense>
     </div>
   );
 }
